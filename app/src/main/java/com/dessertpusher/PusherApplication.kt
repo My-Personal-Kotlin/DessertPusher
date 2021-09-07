1fda7a338c699b9bf8e6266147f4cd322efdc462
Awais Mansha <awaismansha1998@gmail.com>
2021-09-07 15:50:35 +0500

    Working on Lesson 4 in Kotlin

--- app/src/main/java/com/dessertpusher/PusherApplication.kt
+++ app/src/main/java/com/dessertpusher/PusherApplication.kt
@@ -1,4 +1,12 @@
 package com.dessertpusher
 
-class PusherApplication {
+import android.app.Application
+import timber.log.Timber
+
+class PusherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
